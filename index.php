2f87fa50baa2c6dee17b06207f781ecfdf43642c
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-20 13:34:55 +0300

    Add routes

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+// Include the autoloader
+require_once 'autoload.php';
+
+// Load configuration
+require_once 'config/config.php';
+
+// Create the router instance
+$router = new Router(new Request);
+// Define routes
+$router->get('/', 'AuthController@login');
+$router->get('/users', 'UserController@index');
+$router->get('/users/show/{id}', 'UserController@show');
+$router->get('/users/create', 'UserController@create');
+$router->post('/users/store', 'UserController@store');
+$router->get('/users/edit/{id}', 'UserController@edit');
+$router->post('/users/update/{id}', 'UserController@update');
+$router->get('/users/delete/{id}', 'UserController@delete');
+
+$router->get('/login', 'AuthController@login');
+$router->post('/login', 'AuthController@login');
+$router->get('/register', 'AuthController@register');
+$router->post('/register', 'AuthController@register');
+$router->get('/logout', 'AuthController@logout');
+
+$router->get('/', 'AuthController@login');
+// Handle the request
+$router->resolve();
