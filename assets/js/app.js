1f575b7ab7736f1dfcebccf7c74dc0111c1949c1
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-21 12:49:35 +0300

    feat: Add form submission handling for creating departments and users

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,4 +70,99 @@ $(document).ready(function () {
       }
     });
   });
+
+  // handle department form submit
+  $(".department-form").submit(function (event) {
+    event.preventDefault();
+
+    var department = $("#departmentName").val();
+    var department_head_id = $("#departmentHead").val();
+    $.post(
+      "/grade-change/departments/store",
+      {
+        department_name: department,
+        department_head_id: department_head_id,
+      },
+      function (data) {
+        if (data === "success") {
+          $(".alert").show();
+          $(".success").fadeIn();
+          $(".success").html("Department created successfully");
+          $(".success").fadeOut(2000);
+          // reload
+          setTimeout(() => {
+            window.location.reload();
+          }, 2000);
+        } else {
+          $(".alert").fadeIn();
+          $(".error").html(data);
+          $(".alert").fadeOut(2000);
+        }
+      }
+    );
+  });
+
+  //fetch department details for editing
+  $(".edit-department").click(function (e) {
+    e.preventDefault();
+
+    var departmentId = $(this).closest("tr").data("id");
+    $.ajax({
+      url: "/grade-change/department/show",
+      method: "GET",
+      data: { id: departmentId },
+      success: function (response) {
+        let res = JSON.parse(response);
+        let department = res["department"][0];
+        let departmentHead = res["head"][0];
+        console.log(res);
+        console.log(department);
+        console.log(departmentHead);
+        $("#editDepartmentModal #departmentName").val(department.name);
+        if (departmentHead) {
+          $("#editDepartmentModal #departmentHead").val(departmentHead.id);
+        }
+
+        // Show the modal
+        $("#editDepartmentModal").modal("show");
+      },
+      error: function (xhr, status, error) {
+        console.error(error);
+        // Handle error
+      },
+    });
+  });
+
+  // handle new user form submit
+  $("#addUserForm").submit(function (event) {
+    event.preventDefault();
+
+    var fullname = $("#full_name").val();
+    var email = $("#email").val();
+    var user_type = $("#userType").val();
+
+
+    $.post(
+      "/grade-change/users/store",
+      {
+        full_name: fullname,
+        email: email,
+        type: user_type,
+      },
+      function (data) {
+        if (data === "success") {
+          $(".alert").show();
+          $(".success").fadeIn();
+          $(".success").html("User created successfully");
+          $(".success").fadeOut(2000);
+          // reload
+   
+        } else {
+          $(".alert").fadeIn();
+          $(".error").html(data);
+          $(".alert").fadeOut(2000);
+        }
+      }
+    );
+  });
 });
