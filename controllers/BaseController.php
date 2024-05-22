ce7149dca5dbaef565f8a927aafe08c24ae27464
Njoroge Francis Kamande <franciskamande2001@gmail.com>
2024-05-22 16:57:18 +0300

    feat: Add file upload functionality to BaseController

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -46,5 +46,45 @@ class BaseController {
             $this->redirect('/grade-change/login');
         }
     }
+
+    protected function uploadFile($file, $location){
+        $target_dir = 'public/'.$location;
+        $target_file = $target_dir . basename($file["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        $check = getimagesize($file["tmp_name"]);
+        if($check !== false) {
+            $uploadOk = 1;
+        } else {
+            $uploadOk = 0;
+        }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            $uploadOk = 0;
+        }
+        // Check file size
+        if ($file["size"] > 500000) {
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if($imageFileType != "txt" && $imageFileType != "doc" && $imageFileType != "docx") {
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            return false;
+        // if everything is ok, try to upload file
+        } else {
+            echo '<pre>';
+            var_dump($move_uploaded_file($file["tmp_name"], $target_file));
+            echo '</pre>';
+            if (move_uploaded_file($file["tmp_name"], $target_file)) {
+                return $target_file;
+            } else {
+                return false;
+            }
+        }
+    }
 }
 ?>
